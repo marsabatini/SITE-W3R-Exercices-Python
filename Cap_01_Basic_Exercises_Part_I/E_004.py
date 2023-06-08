4da3df1cc4d50d1beb405a28cf65d6d5b185747a
marsabatini <marco_rsabatini@hotmail.com>
2023-06-08 09:07:29 -0300

    V. 0.02

---
+++ Cap_01_Basic_Exercises_Part_I/E_004.py
@@ -0,0 +1,7 @@
+
+#
+
+from math import pi
+
+radius = float(input("Please, input the radius of the circle: "))
+print("The area of the circle with radius " + str(radius) + " is: " + str(pi * radius**2))
