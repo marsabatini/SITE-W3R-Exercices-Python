f7d7c0bf826a819210159e80abb46deaa6de6ab3
marsabatini <marco_rsabatini@hotmail.com>
2023-06-09 09:54:39 -0300

    V. 0.03

--- Cap_01_Basic_Exercises_Part_I/E_012.py
+++ Cap_01_Basic_Exercises_Part_I/E_012.py
@@ -1,6 +1,6 @@
 
     # This code prints the days of the month  from the year and month entered.
-    
+
     # It uses the calendar module allowing the use
     # of several functions of this class.
 
---
+++ Cap_01_Basic_Exercises_Part_I/E_013.py
@@ -0,0 +1,10 @@
+
+    # This is simple program that uses three double quotes
+    # to print a document without losing its tab.
+
+print(""""
+a string that you "don't" have to escape
+This
+is a  ....... multi-line
+heredoc string --------> example
+""")
