6f0fbfecf137b39a7823c098c35fad657da2f7a5
marsabatini <marco_rsabatini@hotmail.com>
2023-06-09 10:07:09 -0300

    V. 0.03

---
+++ Cap_01_Basic_Exercises_Part_I/E_015.py
@@ -0,0 +1,10 @@
+
+    # The program returns the volume of the sphere with radius six.
+
+from math import pi
+
+radius = 6
+
+volume = (4/3)*pi * (radius ** 3)
+
+print("The volume of the sphere with radius 6 is: ", volume)
