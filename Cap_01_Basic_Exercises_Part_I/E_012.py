608751e3c96ce6e951ce326b1a0da13486e8860c
marsabatini <marco_rsabatini@hotmail.com>
2023-06-09 09:50:58 -0300

    V. 0.03

---
+++ Cap_01_Basic_Exercises_Part_I/E_012.py
@@ -0,0 +1,12 @@
+
+    # This code prints the days of the month  from the year and month entered.
+    
+    # It uses the calendar module allowing the use
+    # of several functions of this class.
+
+import calendar
+
+year = int(input("Enter the year: "))
+month = int(input("Enter the month: "))
+
+print(calendar.month(year, month))
