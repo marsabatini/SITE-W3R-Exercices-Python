f9d35c6be53396ad64eeef58dbf259a0d22509a2
marsabatini <marco_rsabatini@hotmail.com>
2023-06-15 16:19:13 -0300

    V. 0.08

---
+++ Cap_01_Basic_Exercises_Part_I/E_063.py
@@ -0,0 +1,8 @@
+
+    # This code gets an absolute file path.
+
+def absoluteFilePath(path):
+    import os
+    return os.path.abspath('path')
+
+print("The absolute file path: ", absoluteFilePath("E_050.py"))
