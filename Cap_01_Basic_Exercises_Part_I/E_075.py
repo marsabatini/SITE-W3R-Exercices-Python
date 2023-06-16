e849440913d07d15fa95af512c68cff3c39b24e3
marsabatini <marco_rsabatini@hotmail.com>
2023-06-16 19:31:18 -0300

    V. 0.09

---
+++ Cap_01_Basic_Exercises_Part_I/E_075.py
@@ -0,0 +1,8 @@
+
+    # The program gets the copyright information and
+    # write Copyright information in Python code.
+
+import sys
+
+print("Python Copyright Information")
+print(sys.copyright)
