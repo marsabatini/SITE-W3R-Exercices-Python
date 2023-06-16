5a8655af21591f7cbaa5c5e656260d13467c4cef
marsabatini <marco_rsabatini@hotmail.com>
2023-06-16 19:25:13 -0300

    V. 0.09

---
+++ Cap_01_Basic_Exercises_Part_I/E_071.py
@@ -0,0 +1,16 @@
+
+    # The program gets a directory listing, sorted by creation date.
+
+from stat import S_ISREG, ST_CTIME, ST_MODE
+import os, sys, time
+
+dirPath = sys.argv[1] if len(sys.argv) == 2 else r'.'
+
+data = (os.path.join(dirPath, fn) for fn in os.listdir(dirPath))
+data = ((os.stat(path), path) for path in data)
+
+data = ((stat[ST_CTIME], path)
+           for stat, path in data if S_ISREG(stat[ST_MODE]))
+
+for cdate, path in sorted(data):
+    print(time.ctime(cdate), os.path.basename(path))
