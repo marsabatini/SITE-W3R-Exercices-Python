7c7ab976c81e86f5dcad565e7fdabdae59d8fe07
marsabatini <marco_rsabatini@hotmail.com>
2023-06-18 11:04:10 -0300

    V. 1.00

---
+++ Cap_01_Basic_Exercises_Part_I/E_083.py
@@ -0,0 +1,9 @@
+
+# The code tests whether all numbers
+# in a list are greater than a certain number.
+
+num = [2, 3, 4, 5]
+
+
+print(all(x > 1 for x in num))
+print(all(x > 4 for x in num))
---
+++ Cap_01_Basic_Exercises_Part_I/E_084.py
@@ -0,0 +1,10 @@
+
+# The program counts the number of occurrences
+# of a specific character in a string.
+
+s = "Essa é apenas mais uma frase de teste de um programa de teste."
+print("Original string:")
+print(s)
+print("Number of occurrence of 'o' in the said string:")
+print(s.count("o"))
+print(s.count("a"))
