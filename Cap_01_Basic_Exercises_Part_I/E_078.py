68b6113aeb95571861745e635c7f3ecadef038ff
marsabatini <marco_rsabatini@hotmail.com>
2023-06-18 10:53:18 -0300

    V. 1.00

---
+++ Cap_01_Basic_Exercises_Part_I/E_078.py
@@ -0,0 +1,8 @@
+
+# The program finds the available built-in modules.
+
+import sys
+import textwrap
+
+module_name = ', '.join(sorted(sys.builtin_module_names))
+print(textwrap.fill(module_name, width=70))
