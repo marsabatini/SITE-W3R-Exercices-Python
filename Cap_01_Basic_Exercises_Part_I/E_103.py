7b34c030c4f4fc3b707c248cee3d34153fd57892
marsabatini <marco_rsabatini@hotmail.com>
2023-06-25 11:52:48 -0300

    V. 1.04

---
+++ Cap_01_Basic_Exercises_Part_I/E_103.py
@@ -0,0 +1,5 @@
+
+# The program extracts the filename from a given path
+
+import os
+print(os.path.basename("home/sabatini/Programação/Python/Sites/W3 Resource/Exercises W3R/Cap_01_Basic_Exercises_Part_I/E_102.py"))
